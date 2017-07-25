8158493c4e288fa7ae8de45e21af9a7d0d8c3e61
Greg Cannon <gregory.cannon@originate.com>
2017-07-25 14:02:17 -0700

    readability tweaks

--- api/controllers/pollController.js
+++ api/controllers/pollController.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose'),
   Poll = mongoose.model('Poll');
 
+// Standard callback
 const standard = (res) => {
   return (function(err, poll) {
     if (err) res.send(err);
@@ -10,6 +11,7 @@ const standard = (res) => {
   });
 };
 
+// Callback with message
 const message = (res, msg) => {
   return (function(err, _) {
     if (err) res.send(err);
@@ -17,9 +19,11 @@ const message = (res, msg) => {
   });
 };
 
+
 exports.list_all_polls = (req, res) => {
   Poll.find({}, standard(res));
 }
+
 
 exports.create_poll = (req, res) => {
   const addCreationDefaults = (poll) => {
@@ -34,33 +38,26 @@ exports.create_poll = (req, res) => {
   new_poll.save(standard(res));
 }
 
+
 exports.read_poll = (req, res) => {
   Poll.findById(req.params.pollId, standard(res));
 }
 
-const sumVotes = (voters) => {
-  let sum = 0;
-  voters.forEach((v) => {
-    sum += v.voteCount;
-  });
-  return sum;
-}
 
 exports.send_votes = (req, res) => {
+  const sumVotes = (voters) => voters.reduce((s, a) => s + a.value, 0);
+
   Poll.findById(req.params.pollId, function(err, currentPoll){
     if (err) res.send(err);
 
     if (currentPoll.status[0] == 'open'){
-      // Add the new votes to each option
+      // Loop through the options and add the new votes
       currentPoll.options.forEach(function(option){
-        const newVote = req.body.options.find(function(d){
-          return d.name == option.name;
-        });
+        const newVote = req.body.options.find(v => (v.name == option.name));
+
         if (newVote != undefined && newVote.voteCount != 0){
           // Remove previous votes by same person
-          option.voters = option.voters.filter((v) => {
-            return (v.voter_name != req.body.voter_name);
-          });
+          option.voters = option.voters.filter(v => (v.voter_name != req.body.voter_name));
 
           option.voters.push({
             voter_name: req.body.voter_name,
@@ -80,6 +77,7 @@ exports.send_votes = (req, res) => {
   });
 }
 
+
 exports.close_poll = (req, res) => {
   Poll.findById(req.params.pollId, function(err, currentPoll){
     if (err) res.send(err);
@@ -89,9 +87,11 @@ exports.close_poll = (req, res) => {
   });
 }
 
+
 exports.delete_poll = (req, res) => {
   Poll.remove({ _id: req.params.pollId }, message(res, 'Removed poll.'));
 }
+
 
 exports.delete_all_polls = (req, res) => {
   Poll.remove({}, message(res, 'Removed all polls.'));
