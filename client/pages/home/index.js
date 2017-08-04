07de7d6b4ffc4a9d3eb892090749d31e97a41ef4
Greg Cannon <gregory.cannon@originate.com>
2017-08-04 16:20:16 -0700

    fixed css stuff

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import './stylesheet.styl';
+
 class Homepage extends Component{
   render() {
     return (
-      <center>
-        <div>
-          <center><h3>You are joining Sigas Lunch Poll</h3></center>
-          <img src="app/images/check.ico" width="50px"/>
-          <form>
-            <input placeholder="enter your name"/>
-            <button>Join</button>
-            <p><font color="red">This name is already used, please enter a different name</font></p>
-            <button>That was me, I want to edit my votes</button>
-          </form>
-        </div>
+      <div>
+        <h3>You are joining Sigas Lunch Poll</h3>
+        <img src="app/images/check.ico" width="50px"/>
+        <form>
+          <input placeholder="enter your name"/>
+          <button>Join</button>
+          <p><font color="red">This name is already used, please enter a different name</font></p>
+          <button>That was me, I want to edit my votes</button>
+        </form>
         <Link to='/poll'>Go To Poll Screen</Link>
-      </center>
+      </div>
     );
   }
 }
