982b9001ae6e9ecda79b469d18e2bdd339527cdb
Cheahuychou Mao <cmao18@mit.edu>
2017-08-04 01:03:44 -0700

    set up create poll

--- client/index.js
+++ client/index.js
@@ -6,8 +6,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Router, Switch, Link} from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-var homepage = require('./pages/home/index.js')
-var poll = require('./pages/poll/index.js')
+import homepage from './pages/home/index.js'
+import poll from './pages/poll/index.js'
+import createPollPage from './pages/create_poll/index.js'
 
 
 class App extends React.Component{
@@ -16,7 +17,7 @@ class App extends React.Component{
       <Router history={createHistory()}>
         <Switch>
           <Route exact path="/" component={homepage}/>
-          <Route path="/poll" component={poll}/>
+          <Route path="/poll" component={createPollPage}/>
         </Switch>
       </Router>
     )
---
+++ client/pages/create_poll/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './stylesheet.styl';
+
+class CreatePollPage extends React.Component {
+  render() {
+    return (
+      <center>Hello World!</center>
+      )
+  }
+}
