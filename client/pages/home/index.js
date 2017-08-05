b189ee8cdb4f8e8dd61402933752bd4cf22f3a4d
Cheahuychou Mao <cmao18@mit.edu>
2017-08-04 17:30:42 -0700

    make the popup to work

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './stylesheet.styl';
+import classnames from 'classnames'
 
 class Homepage extends Component{
   render() {
     return (
       <div className={styles.popup}>
-      <div>
-      <div className={styles.content}>Welcome to</div>
-      <div className={styles.title}>LunchBox</div>
+        <div>
+          <div className={styles.content}>Welcome to</div>
+          <div className={classnames(styles.content, styles.logo)}>LunchBox</div>
+        </div>
+        <hr/>
+        <form>
+          <input type="text" name="username"/>
+        </form>
       </div>
-      <form>
-        <input type="text" name="username"/>
-      </form>
-      </div>
-      );
+    );
   }
 }
 
