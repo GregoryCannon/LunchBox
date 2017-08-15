d32a9ed90b35267966666818e67c68ea787bb89f
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2017-08-14 17:46:06 -0700

    reformat error page (#34)

--- client/pages/error/index.js
+++ client/pages/error/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import NavBar from '../../components/common/navbar'
+import styles from './stylesheet.styl';
+
 const Error = (props) => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <Link to="/">Return Home</Link>
+  	<div>
+	  	<NavBar/>
+	    <div className={styles.content}>
+	      <div className={styles.title}>404 - Page Not Found</div>
+	      <Link to="/">Return Home</Link>
+	    </div>
     </div>
   );
 };
